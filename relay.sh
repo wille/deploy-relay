41df0c696d9479d3ce3ad5ff72f5c490b01c5ec2
redpois0n <red@cmail.nu>
2015-06-16 02:06:42 +0200

    Customized

--- relay.sh
+++ relay.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
-#tor exit-node installer by likvidera
-#add tor rep to apt sourcelist
+
+NICKNAME="rexit"
+CONTACT="red<at>cmail.nu"
+
+# tor exit-node installer by likvidera
+# Add tor repository to apt sourcelist
 echo "deb http://deb.torproject.org/torproject.org wheezy main" >> /etc/apt/sources.list
 echo "deb-src http://deb.torproject.org/torproject.org wheezy main" >> /etc/apt/sources.list
 
-#add key
+# Add key
 gpg --keyserver keys.gnupg.net --recv 886DDD89
 gpg --export A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89 | apt-key add -
 
-                     #update, upgrade and install tor, tor arm and the keyring
+# update, upgrade and install tor, tor arm and the keyring
 apt-get update && apt-get -y upgrade && apt-get -y install tor-arm deb.torproject.org-keyring tor
 
 #download the tor exit notice
-#swehack exit-notice
 wget --no-check-certificate -P /var/run/tor/ https://raw.githubusercontent.com/likvidera/BadOnions/master/SwehackExitNotice/tor-exit-notice.html
 #normal exit-notice
 #wget --no-check-certificate -P /var/run/tor/ https://gitweb.torproject.org/tor.git/plain/contrib/operator-tools/tor-exit-notice.html
@@ -23,43 +26,42 @@ chmod 700 /var/run/tor
 
 #write the tor config, change the changemes!
 cat > /etc/tor/torrc << "TOR_SETTINGS"
-#relay only, no local socks listener please
 SocksPort 0                   
 
 DataDirectory /var/run/tor
 PidFile /var/run/tor/tor.pid
 
 RunAsDaemon 1
-#Don't want to run as root
+# Do not run as root
 User debian-tor 
 
-#tor-arm related stuff
+# ARM related
 ControlSocket /var/run/tor/control
 CookieAuthentication 1
 CookieAuthFileGroupReadable 1             
 CookieAuthFile /var/run/tor/control.authcookie
-#enable this so tor-arm can attach
+# Enable ARM to attach to TOR
 DisableDebuggerAttachment 0            
 
-#Nickname of the tor exit-node
-Nickname CHANGEME
-#Email/Twitter - BTC address (or not)                             
-ContactInfo CHANGEME                     
+# Node name
+Nickname $NICKNAME
+# Contact                          
+ContactInfo $CONTACT                     
 
-#listen on common ports, service needs to start as root
+# Listen on common ports, service needs to start as root
 ORPort 443                        
 DirPort 80
-#our exit-notice served on port 80
+# Display page if visiting using port 80 HTTP
 DirPortFrontPage /var/run/tor/tor-exit-notice.html    
 
-#reduce wear on SSD
+# Less disk usage
 AvoidDiskWrites 1
-#look for OpenSSL hardware cryptographic support               
+#  OpenSSL hardware acceleration    
 HardwareAccel 1                  
-#service must be started as root
+# Service must be started as root
 DisableAllSwap 1               
 PublishServerDescriptor 1
-#exit policies               
+# Exit policies
 Exitpolicy reject *:23
 Exitpolicy reject *:25
 Exitpolicy reject *:109
@@ -79,5 +81,6 @@ Exitpolicy reject *:6881-6999
 Exitpolicy accept *:*
 TOR_SETTINGS
 
+# Reload configuration and start
 service tor reload
 service tor start
